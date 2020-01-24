56a30bc88bc6322416b6d851d8b572635abecced
League Student <league@iMac-16.attlocal.net>
2020-01-23 18:29:28 -0800

    olympic, race(ip)

--- src/_01_Olympic_Rings/OlympicRings_Threaded.java
+++ src/_01_Olympic_Rings/OlympicRings_Threaded.java
@@ -1,8 +1,70 @@
 package _01_Olympic_Rings;
 
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
 
 public class OlympicRings_Threaded {
-	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
-
+	public static void main(String[] args) {
+		Thread r1 = new Thread(()-> {
+			Robot one = new Robot(300, 200);
+			one.setSpeed(10);
+			one.setPenColor(Color.blue);
+			one.penDown();
+			one.setPenWidth(3);
+			for (int i = 0; i < 360; i++) {
+				one.move(3);
+				one.setAngle(i);	
+			}		
+		});
+		Thread r2 = new Thread(()-> {
+			Robot two = new Robot(600, 200);
+			two.setSpeed(10);
+			two.setPenColor(Color.black);
+			two.penDown();
+			two.setPenWidth(3);
+			for (int i = 0; i < 360; i++) {
+				two.move(3);
+				two.setAngle(i);	
+			}
+		});
+		Thread r3 = new Thread(()-> {
+			Robot three = new Robot(900, 200);
+			three.setSpeed(10);
+			three.setPenColor(Color.red);
+			three.moveTo(1);
+			three.setPenWidth(3);
+			for (int i = 0; i < 360; i++) {
+				three.move(3);
+				three.setAngle(i);	
+			}
+		});
+		Thread r4 = new Thread(()-> {
+			Robot four = new Robot(450, 500);
+			four.setSpeed(10);
+			four.setPenColor(Color.yellow);
+			four.penDown();
+			four.setPenWidth(3);
+			for (int i = 0; i < 360; i++) {
+				four.move(3);
+				four.setAngle(i);	
+			}
+		});
+		Thread r5 = new Thread(()-> {
+			Robot five = new Robot(750, 500);
+			five.setSpeed(10);
+			five.setPenColor(Color.green);
+			five.penDown();
+			five.setPenWidth(3);
+			for (int i = 0; i < 360; i++) {
+				five.move(3);
+				five.setAngle(i);	
+			}
+		});
+		r1.start();
+		r2.start();
+		r3.start();
+		r4.start();
+		r5.start();
+	}
 }
-
--- src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
+++ src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
@@ -1,6 +1,7 @@
 package _02_Advanced_Robot_Race;
 
 public class AdvancedRobotRace {
-	// Re-do the robot race recipe from level 3 module 0. 
-	// This time, use threads to make all of the robots go at the same time.
+	public static void main(String[] args) {
+		//make robots move random distance and determine winners
+	}
 }
